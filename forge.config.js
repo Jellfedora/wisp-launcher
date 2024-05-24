c366dd52d315cc688912d33cfa6fa2897099916b
gfjuhgv <j.lecointe@caplaser.fr>
2024-05-24 16:28:01 +0200

    Maj icon app

--- forge.config.js
+++ forge.config.js
@@ -12,21 +12,30 @@ module.exports = {
       name: '@electron-forge/maker-squirrel',
       config: {
         loadingGif: "./public/install-gif.gif",
-        setupIcon: "./public/favicon.ico"
+        setupIcon: "./public/icon.ico",
+        authors: 'Wisp Forge',
+        description: 'Wisp launcher for Valheim mods'
+        //iconUrl: 'http://example.com/icon.ico'
       }
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          //maintainer: 'Wisp Forge',
+          //description: 'Wisp launcher for Valheim mods',
+          //icon: "./public/icon.ico"
+        }
+      },
     },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
     },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    //{
+    //  name: '@electron-forge/maker-rpm',
+    //  config: {},
+    //},
   ],
   plugins: [
     {
